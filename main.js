464626f1dfb58fde24b050698f687b2654e34eb0
Eli <150296902+Insane-Eli@users.noreply.github.com>
2026-02-04 12:58:08 -0800

    restarted some files
    some of the comments and code i wrote from like 2 years ago are so blatantly copy and pasted from chatgpt that i can't just pick up where i left off in good conscience lmao.

--- main.js
+++ main.js
@@ -1,8 +1,8 @@
 window.onload = function() {
-    console.log("Page is fully loaded, and now this script runs automatically.");
-        fetch('header.html') // Load the header HTML file
+    console.log("page fully loaded");
+        fetch('header.html')
             .then(response => response.text())
             .then(data => {
-                document.getElementById('header').innerHTML = data; // Insert the header HTML into the page
+                document.getElementById('header').innerHTML = data;
         });
 };
