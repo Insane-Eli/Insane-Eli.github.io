85495fb290b2d503243e265c6a0cd2c28c1fa056
Eli <150296902+Insane-Eli@users.noreply.github.com>
2026-02-06 10:14:10 -0800

    age counter :O

--- frontend/js/main.js
+++ frontend/js/main.js
@@ -4,9 +4,25 @@ function loadHeader(){
         .then(response => response.text())
         .then(data => {
             document.getElementById('header').innerHTML = data;
+            getAge();
     });
 }
 
 window.onload = function() {
     loadHeader();
 };
+
+const age = document.getElementById("age");
+const ageDec = document.getElementById("ageDecimal");
+const birthday = new Date("2008-04-22");
+
+function getAge (){
+    const now = new Date();
+    const years = (now - birthday) / (1000 * 60 * 60 * 24 * 365.2425); // .2425 cause of leap years
+    age.textContent = Math.floor(years);
+    ageDec.textContent = '.' + years.toFixed(9).slice(3);
+}
+
+getAge();
+setInterval(getAge,50);
+
